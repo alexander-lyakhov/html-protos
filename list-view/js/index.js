4adf2848ac5e41c88b450fa802b20e27a5161446
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2022-01-30 16:59:18 +0200

    ...

---
+++ list-view/js/index.js
@@ -0,0 +1,16 @@
+﻿(function() {
+  console.log('!!!')
+
+  document.body.addEventListener('contextmenu', e => {
+    e.preventDefault()
+    return false
+  })
+
+  document.body.addEventListener('mousedown', e => {
+    console.log(e)
+    if (e.button === 2) {
+      console.log(123)
+    }
+  })
+
+})()
